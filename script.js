27066b3f064567ad6cabcec062f6d647ee37143b
unknown <mikaelmarkus1000@gmail.com>
2021-09-29 20:50:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Varibles for this to work
+
+str = 0 
+int = 0
+con = 0
+wis = 0
+cha = 0
+dex = 0
+hp = 0
+hpm = 0
+pClass=""
+hitdie =""
+// Varibles that need math  
+
+acro = 0
+ani = 0
+arca = 0
+hth = 0
+dec = 0
+his = 0
+ins = 0
+intd = 0
+inves = 0
+med = 0
+natu = 0
+perpt = 0
+pref = 0
+pers = 0
+rel = 0
+sligh = 0
+sth = 0
+surv = 0 
+
+
